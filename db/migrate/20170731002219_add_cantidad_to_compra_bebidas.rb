2bbc10c7b3721cdd0bd17d2222748fd25bfe7df6
Claudio Pellegrini <ladeclaudio@gmail.com>
2017-08-03 19:46:36 -0300

    barcode sumar valorticketfinal + bebida, comento migracion cantidad compra bebidas

--- db/migrate/20170731002219_add_cantidad_to_compra_bebidas.rb
+++ db/migrate/20170731002219_add_cantidad_to_compra_bebidas.rb
@@ -1,5 +1,5 @@
 class AddCantidadToCompraBebidas < ActiveRecord::Migration[5.0]
   def change
-    add_column :compra_bebidas, :cantidad, :integer
+    # add_column :compra_bebidas, :cantidad, :integer
   end
 end
